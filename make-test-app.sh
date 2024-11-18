f6afae9b92ef5d9b718a11390fb9e292d26b70cf
yonis-savary <yonissavary@outlook.fr>
2024-11-18 22:17:45 +0100

    manual test app : better path

--- make-test-app.sh
+++ make-test-app.sh
@@ -1,6 +1,6 @@
-mkdir test-app
-echo "{\"require\": {\"yonis-savary/sharp\": \"dev-main\"}, \"repositories\": [{\"type\": \"path\", \"url\": \"$(pwd)\"}]}" > ./test-app/composer.json
-cd test-app
+mkdir tests/Manual-test-app
+echo "{\"require\": {\"yonis-savary/sharp\": \"dev-main\"}, \"repositories\": [{\"type\": \"path\", \"url\": \"$(pwd)\"}]}" > ./tests/Manual-test-app/composer.json
+cd tests/Manual-test-app
 composer install
 composer exec sharp-install
 php do initialize
